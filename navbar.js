c38e9517bc45d1c61831aa196c2b49cc8dd1999e
shinymoth97 <157185514+shinymoth97@users.noreply.github.com>
2024-01-22 14:08:46 +1100

    Add files via upload

--- navbar.js
+++ navbar.js
@@ -1,7 +1,7 @@
 const navbarCode = `
-<nav>
+<nav class="topnav" id="myTopnav">
 
-<a href="index.html" class="nav-link">
+<a href="index.html" class="nav-link first">
 <div class="container">
 <img src="images/Logotrim.png" class="image">
 <div class="text">
@@ -28,6 +28,18 @@ Home</div>
 <a href="contact.html" class="dropbtn nav-link">Contact</a>
 
 <a href="#" class="nav-link">About</a>
+
+<a href="javascript:void(0);" class="icon nav-link" onclick="myFunction()">
+    <i class="fa fa-bars"></i></a>
 </nav>
 `;
 document.getElementById('navbarContainer').innerHTML = navbarCode;
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
